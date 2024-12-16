533043cba986ee607cdbb187897245e865a065a3
masa <masa@masanoMacBook-Pro.local>
2024-12-16 21:20:56 +0900

    test

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # 開発用サーバを起動
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
